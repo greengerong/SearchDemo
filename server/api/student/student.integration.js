af1724c2f1058843fbfef9714554a1e4820423d3
polang <grzx2210@163.com>
2016-05-31 16:45:44 +0800

    build script

--- server/api/student/student.integration.js
+++ server/api/student/student.integration.js
@@ -9,41 +9,42 @@ let schemaValidator = new jsonschema.Validator();
 
 describe('Student API:', () => {
 
-  it('should got matched student when given search name query', (done) => {
-    let name = 'ran';
+    it('should got matched student when given search name query', (done) => {
+        let name = 'ran';
 
-    request(app)
-      .get('/api/student?name=' + name)
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        (!err).should.be.true;
-        schemaValidator.validate(res.body, studentSchema).valid.should.be.true;
-        done();
-      });
-  });
+        request(app)
+            .get('/api/student?name=' + name)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                (!err).should.be.true;
+                schemaValidator.validate(res.body, studentSchema).valid.should.be.true;
+                res.body.data[0].name.should.be.equal('Liu Ran');
+                done();
+            });
+    });
 
-  it('should got all student when given search name is empty', (done) => {
-    let name = '';
+    it('should got all student when given search name is empty', (done) => {
+        let name = '';
 
-    request(app)
-      .get('/api/student?name=' + name)
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        (!err).should.be.true;
-        schemaValidator.validate(res.body, studentSchema).valid.should.be.true;
-        res.body.data.length.should.be.equal(res.body.total);
-        done();
-      });
-  });
+        request(app)
+            .get('/api/student?name=' + name)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                (!err).should.be.true;
+                schemaValidator.validate(res.body, studentSchema).valid.should.be.true;
+                res.body.data.length.should.be.equal(res.body.total);
+                done();
+            });
+    });
 
-  afterEach((done) => {
-    let server = app.get('server');
-    if (!server.listening) {
-      return done();
-    }
-    server.close(done);
-  });
+    afterEach((done) => {
+        let server = app.get('server');
+        if (!server.listening) {
+            return done();
+        }
+        server.close(done);
+    });
 
 });
